94dc2961812e62dd49ff01dfb6dc14217bfe53f6
Sagar Tandan <076bct072@ioepc.edu.np>
2024-06-20 21:55:52 +0545

    update firebase

--- src/config.jsx
+++ src/config.jsx
@@ -5,13 +5,22 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_LVSeCE0rfJSP8XhJDhcKEkdbhzN-w18",
-  authDomain: "npm-project-d3999.firebaseapp.com",
-  projectId: "npm-project-d3999",
-  storageBucket: "npm-project-d3999.appspot.com",
-  messagingSenderId: "406497186944",
-  appId: "1:406497186944:web:d4fa1c9d67aac8d4834265",
-  measurementId: "G-TVBT35JW40",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSESING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+
+  // apiKey: "AIzaSyC_LVSeCE0rfJSP8XhJDhcKEkdbhzN-w18",
+  // authDomain: "npm-project-d3999.firebaseapp.com",
+  // projectId: "npm-project-d3999",
+  // storageBucket: "npm-project-d3999.appspot.com",
+  // messagingSenderId: "406497186944",
+  // // appId: "1:406497186944:web:d4fa1c9d67aac8d4834265",
+  // // measurementId: "G-TVBT35JW40",
+
 };
 
 // Initialize Firebase
