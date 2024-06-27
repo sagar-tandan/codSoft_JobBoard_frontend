05e8b2a1e903682d43c17f95398320d986e1375a
Sagar Tandan <076bct072@ioepc.edu.np>
2024-06-27 13:30:10 +0545

    navbar for company page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,31 @@ export default function Navbar() {
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
   const navigate = useNavigate();
 
+  // Get date
+
+  let date = new Date(); // Current date and time
+
+  let year = date.getFullYear();
+  let monthIndex = date.getMonth(); // Getting the month index (0-11)
+  let day = date.getDate();
+
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let monthName = monthNames[monthIndex];
+
   useEffect(() => {
     const verifyCookie = async () => {
       if (!cookies.token) {
@@ -56,60 +81,24 @@ export default function Navbar() {
                 <h1 className="py-1">JobBoard</h1>
               </div>
             </Link>
+
+            <div className="w-full font-bold font-poppins text-xl lg:text-2xl tracking-wide flex justify-center items-center">
+              <h1>{`${day}, ${monthName}, ${year} `}</h1>
+            </div>
             <div className="flex gap-2 lg:gap-4 py-3 items-center font-medium lg:font-semibold">
-              {userName ? (
-                <div className="flex gap-3 items-center">
-                  {/* <div className="bg-green-500 py-1 px-3 text-white rounded hover:cursor-pointer hover:bg-green-600 transition-all ease-in-out duration-300">Post a Job</div> */}
-                  <Link to="/">
-                    <img
-                      onClick={() => {
-                        Logout();
-                      }}
-                      className="w-7 h-7"
-                      src={profile}
-                      alt=""
-                    />
-                  </Link>
-                </div>
-              ) : (
-                <div className="relative flex gap-4">
-                  <Link to="/login">
-                    <h1 className="hidden sm:inline-block hover:cursor-pointer border-[2px] px-6 lg:px-8 py-1 rounded-full border-[#c1c1c1] hover:bg-green-600 hover:border-green-600 hover:text-white transition-all ease-in-out duration-300">
-                      Login
-                    </h1>
-                  </Link>
-                  <div
-                    onClick={() => {
-                      toggleButton();
-                    }}
-                    className="hidden sm:flex w-full gap-2 hover:cursor-pointer border-[2px] px-3 sm:px-6 lg:px-7 sm:py-1 rounded-full text-white border-green-600 bg-green-600 hover:bg-green-700  hover:border-green-700 transition-all ease-in-out duration-300"
-                  >
-                    <h1 className="w-full ">Register </h1>
-                    {/* <h1 className="w-full ">▼</h1> */}
-                  </div>
-                  {/* </Link> */}
-                  {register && (
-                    <div className="dropdownAnimation flex flex-col gap-2 py-2 absolute top-10 justify-center items-center bg-[#f2f2f2] shadow-md w-full">
-                      <Link to="/registeruser">
-                        <h1
-                          onClick={() => toggleButton()}
-                          className="hover:cursor-pointer hover:text-green-600"
-                        >
-                          Register JobSeeker
-                        </h1>
-                      </Link>
-                      <Link to="/registercompany">
-                        <h1
-                          onClick={() => toggleButton()}
-                          className="hover:cursor-pointer hover:text-green-600"
-                        >
-                          Register Company
-                        </h1>
-                      </Link>
-                    </div>
-                  )}
-                </div>
-              )}
+              <div className="flex gap-3 items-center">
+                {/* <div className="bg-green-500 py-1 px-3 text-white rounded hover:cursor-pointer hover:bg-green-600 transition-all ease-in-out duration-300">Post a Job</div> */}
+                {/* <Link to="/"> */}
+                <img
+                  // onClick={() => {
+                  //   Logout();
+                  // }}
+                  className="w-8 h-8 object-cover "
+                  src={userName.image}
+                  alt="profile"
+                />
+                {/* </Link> */}
+              </div>
 
               <div className="sm:hidden inline-block hover:cursor-pointer transition-all ease-in-out duration-300">
                 <img className="w-6 h-6" src={hamburger} alt="" />
