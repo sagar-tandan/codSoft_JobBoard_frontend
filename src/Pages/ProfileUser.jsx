c6f1335b599b83fabbdb2aae4e8bd328459026a3
Sagar Tandan <076bct072@ioepc.edu.np>
2024-06-30 22:00:11 +0545

    rollback

--- src/Pages/ProfileUser.jsx
+++ src/Pages/ProfileUser.jsx
@@ -54,7 +54,6 @@ export default function ProfileUser({ data }) {
             </div>
 
             <img
-              onClick={(e) => setIsTodoEditable(true)}
               className="w-4 h-4"
               src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
               alt=""
